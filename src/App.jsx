2b49765fb16c190a379b54973393a93e2b5f89bf
Gab-Martins <132864619+Gab-Martins@users.noreply.github.com>
2023-11-13 23:50:03 +0000

    Add tags

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ function App() {
     <>
       <div className='card'>
         <h2>Ricky Sanchez</h2>
+          <div className='tags'>
+            <div className='tag'>Tag 1</div>
+            <div className='tag'>Tag 2</div>
+            <div className='tag'>Tag 3</div>
+          </div>
           <img src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>
     </>
