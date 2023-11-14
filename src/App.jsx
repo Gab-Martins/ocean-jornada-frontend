5f77cd2c6f1398fd91a3b865a395b7d2388e803b
Gab-Martins <132864619+Gab-Martins@users.noreply.github.com>
2023-11-14 00:46:24 +0000

    Component Card em função

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
 
   return (
     <>
-      <div className='card'>
-        <h2>Ricky Sanchez</h2>
-          <div className='tags'>
-            <div className='tag'>Tag 1</div>
-            <div className='tag'>Tag 2</div>
-            <div className='tag'>Tag 3</div>
-          </div>
-          <img src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-      </div>
+      <br />
+      <Card />
+      <br />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+    return <div className='card'>
+    <h2>Ricky Sanchez</h2>
+      <div className='tags'>
+        <div className='tag'>Tag 1</div>
+        <div className='tag'>Tag 2</div>
+        <div className='tag'>Tag 3</div>
+      </div>
+      <img src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+  </div>
+}
