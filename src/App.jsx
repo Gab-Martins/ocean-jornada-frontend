d14784eea2318472f1ecc80668a7d75d257a3e91
Gab-Martins <132864619+Gab-Martins@users.noreply.github.com>
2023-11-15 01:14:30 +0000

    Nova página p/ personagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,15 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Card from './components/Card/Card'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
+import ReadById from './components/ReadById/ReadById';
 
-function App() {
-  const item1 = {
-    name: "Rick Sanchez",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    tags: ["Status:Vivo", "AAA:aaa", "BBB:bbb"]
-  }
-  const item2 = {
-    name: " Morty",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-}
-  const item3 = {
-    name: "Summer Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-}
-  //const itens = [item1, item2, item3]
-
-  const[itens, setItens] = useState([])
-
-  async function carregarDadosApi(){
-    const apiUrl = "https://rickandmortyapi.com/api/character/"
-
-    const response = await fetch(apiUrl)
-
-    const body = await response.json()
-
-    setItens(body.results)
-  }
-
-  useEffect(function (){
-    carregarDadosApi()
-  }, [])
-
+export default function App() {
   return (
-    <>
-      <div className='cards'>
-      {itens.map(function(item){
-        return <Card item = {item}/>
-      })}
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/:id' element={<ReadById/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
